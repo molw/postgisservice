6b010b6f45d47b738ea67d1fa3aec0d1146b70ba
Steven Pousty <spousty@redhat.com>
2017-01-13 14:08:24 -0800

    Helps if you extend the right class

--- src/main/java/org/molw/ws/ApplicationRoot.java
+++ src/main/java/org/molw/ws/ApplicationRoot.java
@@ -1,12 +1,13 @@
 package org.molw.ws;
 
 import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
 
 /**
  * Created by steve on 1/13/2017.
  */
 @ApplicationPath("ws")
-public class ApplicationRoot {
+public class ApplicationRoot extends Application {
 
 
 }
